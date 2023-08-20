08bc3f08e8d64da7b78f9271164f4de0a80024fc
Serhii <66913762+Serhii-vishn@users.noreply.github.com>
2023-08-20 17:33:55 +0300

    Add project files.

---
+++ CCalculator/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CCalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\t\tWelcome to the simplest console calculator app");
+            Console.WriteLine("\t--------------------------------------------------------------");
+        }
+    }
+}
