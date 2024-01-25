e567dd57e350fe29cf3f3ca5cc413c74df3edf5d
Serhii <66913762+Serhii-vishn@users.noreply.github.com>
2024-01-25 21:33:47 +0200

    add logic

---
+++ CCalculator/Calculator.cs
@@ -0,0 +1,25 @@
+﻿namespace CCalculator
+{
+    public static class Calculator
+    {
+        public static  double Plus(double frstOperand, double scndOperand)
+        {
+            return frstOperand + scndOperand;
+        }
+
+        public static double Minus(double frstOperand, double scndOperand)
+        {
+            return frstOperand - scndOperand;
+        }
+
+        public static double Multiply(double frstOperand, double scndOperand)
+        {
+            return frstOperand * scndOperand;
+        }
+
+        public static double Divide(double frstOperand, double scndOperand)
+        {
+            return frstOperand / scndOperand;
+        }
+    }
+}
--- CCalculator/Program.cs
+++ CCalculator/Program.cs
@@ -1,17 +1,67 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CCalculator
 {
     internal class Program
     {
-        static void Main(string[] args)
+        public static void Main()
         {
             Console.WriteLine("\t\tWelcome to the simplest console calculator app");
             Console.WriteLine("\t--------------------------------------------------------------");
+
+            double frstOperand = GetOperand("Entear value: ");
+            double scndOperand = GetOperand("Entear value: ");
+
+            double result = 0.0;
+
+            char operation = GetOperation("Operation: ");
+            switch (operation)
+            {
+                case '+':
+                    result = Calculator.Plus(frstOperand, scndOperand);
+                    break;
+                case '-':
+                    result = Calculator.Minus(frstOperand, scndOperand);
+                    break;
+                case '*':
+                    result = Calculator.Multiply(frstOperand, scndOperand);
+                    break;
+                case '/':
+                    result = Calculator.Divide(frstOperand, scndOperand);
+                    break;
+                default:
+                    Console.WriteLine("Invalid operation. Try again");
+                    break;
+            }
+            Console.WriteLine($"\n\nResult: {result}");
+
+            Console.ReadKey();
+        }
+
+        public static double GetOperand(string message)
+        {
+            while (true)
+            {
+                Console.Write(message);
+                if (Double.TryParse(Console.ReadLine(), out double operand))
+                    return operand;
+
+                Console.WriteLine("Invalid value. Please enter a number");
+            }
+        }
+
+        public static char GetOperation(string message)
+        {
+            while (true)
+            {
+                Console.Write(message);
+
+                char operation = Console.ReadKey().KeyChar;
+                if(operation.Equals('+') || operation.Equals("-")|| operation.Equals('*') || operation.Equals("/"))
+                    return operation;
+
+                Console.WriteLine("Invalid value. Please enter a number");
+            }
         }
     }
 }
